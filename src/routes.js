c4072c67cce7fb47402aa47320f14faa84f86f37
Jean Carlos Guimarães <mir_jcosta@uolinc.com>
2021-11-26 12:01:27 -0300

    Adding route '/master'

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Master from './pages/Master';
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/master" component={Master} />
+      </Switch>
     </BrowserRouter>
   );
 }
