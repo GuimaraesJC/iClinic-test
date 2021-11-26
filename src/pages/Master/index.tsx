d7d59cc605766529e2a5e020264cff8895cab8b9
Jean Carlos Guimarães <mir_jcosta@uolinc.com>
2021-11-26 15:40:12 -0300

    Adding API calls

--- src/pages/Master/index.tsx
+++ src/pages/Master/index.tsx
@@ -1,17 +1,34 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import * as S from './styles';
 
 import arrowLeft from '../../assets/icons/arrow-left.svg';
 
 function Master() {
+  const [masterName, setMasterName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    Promise.race([
+      fetch('https://swapi.dev/api/people/1'),
+      fetch('https://swapi.dev/api/people/4')
+    ])
+      .then(response => response.json())
+      .then(data => setMasterName(data.name));
+  }, []);
+
   return (
     <S.Container>
+      <Link to="/">
       <S.ReturnContainer>
         <img src={arrowLeft} alt="Back" />
         <span>back</span>
       </S.ReturnContainer>
+      </Link>
       <S.Button>choose your path again, Padawan</S.Button>
       <S.ImageContainer></S.ImageContainer>
-      <S.MasterInfo>Your master is <span>Darth Vader</span></S.MasterInfo>
+      <S.MasterInfo>Your master is <span>{masterName}</span></S.MasterInfo>
     </S.Container>
   );
 }
