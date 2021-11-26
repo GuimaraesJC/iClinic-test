c1ecde07f2606be3e6b9f208f0243cdd1a9fd619
Jean Carlos Guimarães <mir_jcosta@uolinc.com>
2021-11-26 10:01:14 -0300

    Main screen layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import * as S from './styles/App.styles'
+
 function App() {
   return (
-    <h1>Home</h1>
+    <S.Container>
+      <S.Title>Welcome to <span>iClinic</span></S.Title>
+      <S.Subtitle>Frontend challenge - by <span>Jean Carlos Guimarães</span></S.Subtitle>
+
+      <S.Button>Start</S.Button>
+    </S.Container>
   );
 }
 
---
+++ src/styles/App.styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #2c97d1;
+`;
+
+export const Title = styled.h1`
+  font-weight: normal;
+  font-size: 72px;
+  line-height: 88px;
+  margin-top: 210px;
+
+  span {
+    font-family: 'Lato', sans-serif;
+    font-weight: bold;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 0.35em;
+  text-transform: uppercase;
+
+  span {
+    font-weight: bold;
+  }
+`;
+
+export const Button = styled.button`
+  width: 189px;
+  height: 56px;
+  background: #2c97d1;
+  border: none;
+  border-radius: 10px;
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 22px;
+  letter-spacing: 0.35em;
+  text-transform: uppercase;
+  margin-top: 162px;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
